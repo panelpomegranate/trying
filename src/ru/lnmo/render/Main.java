bce616c81b8de26547836f313407fb825701b238
panelpomeranate <panelpomegranate@gmail.com>
2020-10-22 18:27:30 +0300

    vectors, obj

--- src/ru/lnmo/render/Main.java
+++ src/ru/lnmo/render/Main.java
@@ -1,6 +1,5 @@
 package ru.lnmo.render;
 
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -25,7 +24,7 @@ public class Main extends JFrame {
     static final int h = 786;
 
     public static void draw(Graphics2D g) throws IOException {
-        String fileName = "C:/Users/leoso/Downloads/uaz.txt";
+        String fileName = "uaz_1.txt";
         BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 
 // String s="0";
@@ -40,103 +39,100 @@ public class Main extends JFrame {
         for (String l : readUsingScanner(fileName)) {
             k++;
             l.split(" ");
-
+            if(l.isEmpty())continue;
             if (l.length() != 0) {
-                if (l.charAt(0) == 'v' && l.charAt(1) == ' ') {
+                if (l.startsWith("v ")) {
                     kk = kk + 1;
                     String s = "";
-                    int qq = 0;
-                    for (int i = 2; i < l.length(); i++) {
-                        if (l.charAt(i) != ' ') {
-                            s = s + l.charAt(i);
-                        } else {
-                            u[kk][qq] = Double.parseDouble(s);
-                            s = "";
-                            qq = qq + 1;
-                        }
-                    }
-                    u[kk][qq] = Double.parseDouble(s);
-                    s = "";
-                    qq = qq + 1;
-                }
 
-                if (l.charAt(0) == 'v' && l.charAt(1) == 't') {
-                    kk = 0;
-                    String s = "";
-                    int qq = 0;
-                    for (int i = 3; i < l.length(); i++) {
-                        if (l.charAt(i) != ' ') {
-                            s = s + l.charAt(i);
-                        } else {
-                            vt[i][qq] = Double.parseDouble(s);
-                            s = "";
+                    String parts[] = l.split(" ");
+                    double x = Double.parseDouble(parts[1]);
+                    double y = Double.parseDouble(parts[2]);
+                    double z = Double.parseDouble(parts[3]);
 
-                            qq = qq + 1;
-                        }
-                    }
-
-                }
-                if (l.charAt(0) == 'v' && l.charAt(1) == 'n') {
-
-                    String s = "";
-                    int qq = 0;
-                    for (int i = 3; i < l.length(); i++) {
-                        if (l.charAt(i) != ' ') {
-                            s = s + l.charAt(i);
-                        } else {
-                            vn[i][qq] = Double.parseDouble(s);
-                            s = "";
-
-                            qq = qq + 1;
-                        }
-                    }
-
-                }
-                if (l.charAt(0) == 'f' && l.charAt(1) == ' ') {
-                    kk = kk + 1;
-                    String s = "";
-                    int qq = 0;
-                    int ss = 0;
-                    for (int i = 2; i < l.length(); i++) {
-
-                        if (l.charAt(i) != ' ' && l.charAt(i) != '/') {
-                            s = s + l.charAt(i);
-                        }
-                        if (l.charAt(i) == '/') {
-                            if (qq == 0) {
-                                ff[ss] = Integer.parseInt(s);
-                            }
-                            qq = qq + 1;
-                        }
-                        if (l.charAt(i) == ' ') {
-                            ss = ss + 1;
-                            s = "";
-                            qq = 0;
-                        }
-                    }
-                    int hh = ff[0];
-                    double x4 = u[hh][0] + 300;
-                    double y4 = u[hh][1] + 300;
-                    double z4 = u[hh][2] + 300;
-                    hh = ff[1];
-                    double x5 = u[hh][0] + 300;
-                    double y5 = u[hh][1] + 300;
-                    double z5 = u[hh][2] + 300;
-                    hh = ff[2];
-                    double x6 = u[hh][0] + 300;
-                    double y6 = u[hh][1] + 300;
-                    double z6 = u[hh][2] + 300;
-                    Render.renderTriangle(img, x4, y4, x5, y5, x6, y6, z4, z5, z6);
+                    u[kk][0] = x;
+                    u[kk][1] = y;
+                    u[kk][2] = z;
                 }
             }
+
+            if (l.charAt(0) == 'v' && l.charAt(1) == 't') {
+                kk = 0;
+                String s = "";
+                int qq = 0;
+                for (int i = 3; i < l.length(); i++) {
+                    if (l.charAt(i) != ' ') {
+                        s = s + l.charAt(i);
+                    } else {
+                        vt[i][qq] = Double.parseDouble(s);
+                        s = "";
+
+                        qq = qq + 1;
+                    }
+                }
+
+            }
+            if (l.charAt(0) == 'v' && l.charAt(1) == 'n') {
+
+                String s = "";
+                int qq = 0;
+                for (int i = 3; i < l.length(); i++) {
+                    if (l.charAt(i) != ' ') {
+                        s = s + l.charAt(i);
+                    } else {
+                        vn[i][qq] = Double.parseDouble(s);
+                        s = "";
+
+                        qq = qq + 1;
+                    }
+                }
+
+            }
+            if (l.charAt(0) == 'f' && l.charAt(1) == ' ') {
+                String tris [] = l.split(" ");
+                int v1Id = Integer.parseInt(tris[1].split("/")[0]);
+                int v2Id = Integer.parseInt(tris[2].split("/")[0]);
+                int v3Id = Integer.parseInt(tris[3].split("/")[0]);
+/* kk = kk + 1;
+String s = "";
+int qq = 0;
+int ss = 0;
+for (int i = 2; i < l.length(); i++) {
+
+if (l.charAt(i) != ' ' && l.charAt(i) != '/') {
+s = s + l.charAt(i);
+}
+if (l.charAt(i) == '/') {
+if (qq == 0) {
+ff[ss] = Integer.parseInt(s)-1;
+}
+qq = qq + 1;
+}
+if (l.charAt(i) == ' ') {
+ss = ss + 1;
+s = "";
+qq = 0;
+}
+}
+*/ int hh = v1Id;
+                double x4 = u[hh][0] + 300;
+                double y4 = u[hh][1] + 300;
+                double z4 = u[hh][2] + 300;
+                hh = v2Id;
+                double x5 = u[hh][0] + 300;
+                double y5 = u[hh][1] + 300;
+                double z5 = u[hh][2] + 300;
+                hh = v3Id;
+                double x6 = u[hh][0] + 300;
+                double y6 = u[hh][1] + 300;
+                double z6 = u[hh][2] + 300;
+                Render.renderTriangle(img, x4, y4, z4, x5, y5, z5, x6, y6, z6);
+            }
         }
+        g.drawImage(img, 0, 0, null);
     }
 
-
-
-
-
-        //магический код позволяющий всему работать, лучше не трогать
+    //магический код позволяющий всему работать, лучше не трогать
     public static void main(String[] args) throws InterruptedException, IOException {
         Main jf = new Main();
         jf.setSize(w, h);//размер экрана
@@ -145,9 +141,9 @@ public class Main extends JFrame {
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.createBufferStrategy(2);
-        //в бесконечном цикле рисуем новый кадр
+//в бесконечном цикле рисуем новый кадр
         while (true) {
-            long frameLength = 1000 / 60; //пытаемся работать из рассчета  60 кадров в секунду
+            long frameLength = 1000 / 60; //пытаемся работать из рассчета 60 кадров в секунду
             long start = System.currentTimeMillis();
             BufferStrategy bs = jf.getBufferStrategy();
             Graphics2D g = (Graphics2D) bs.getDrawGraphics();
--- src/ru/lnmo/render/Render.java
+++ src/ru/lnmo/render/Render.java
@@ -7,52 +7,40 @@ import static java.lang.Math.*;
 
 public class Render {
 
-    public static void render(BufferedImage img){
-//        img.setRGB(500, 300, new Color(255, 0, 200).getRGB());
+    public static void render(BufferedImage img) {
         for (int i = 0; i < img.getWidth(); i++) {
             for (int j = 0; j < img.getHeight(); j++) {
-                img.setRGB(i, j, new Color(i * j % 256, (i + j) % 256, (i * i + j * j) % 256).getRGB() );
+                img.setRGB(i, j, new Color(i * j % 256, (i + j) % 256, (i * i + j * j) % 256).getRGB());
             }
         }
     }
 
-    //Стоит начать с этого
-    public static void renderLine(BufferedImage img, int x1, int y1, int x2, int y2){
-        for (int i = 0; i < 1366; i++) {
-            for (int j = 0; j < 768; j++) {
-                if(j==(((i-x1)*(y2-y1))/(x2-x1))+y1){
-                    img.setRGB(i, j, new Color(0, 0, 0).getRGB() );
-                }
-            }
-        }
-    }
-
-    public static void renderLine1(BufferedImage img, int x1, int y1, int x2, int y2){
-        if(x1!=x2){
-            double k=((double)(y1-y2))/((double)(x1-x2));
-            if((x1<x2)&(y1>=y2)){
-                if(y1!=y2){
-                    int y=0;
+    public static void renderLine(BufferedImage img, int x1, int y1, int x2, int y2) {
+        if (x1 != x2) {
+            double k = ((double) (y1 - y2)) / ((double) (x1 - x2));
+            if ((x1 < x2) & (y1 >= y2)) {
+                if (y1 != y2) {
+                    int y = 0;
                     for (int i = min(x1, x2); i < max(x1, x2); i++) {
                         for (int j = min(y1, y2); j < max(y1, y2); j++) {
-                            if(j==(((i-x1)*(y2-y1))/(x2-x1))+y1){
-                                y=j;
-                                img.setRGB(i, j, new Color(0, 0, 0).getRGB() );
-                            }else {
+                            if (j == (((i - x1) * (y2 - y1)) / (x2 - x1)) + y1) {
+                                y = j;
+                                img.setRGB(i, j, new Color(0, 0, 0).getRGB());
+                            } else {
                                 img.setRGB(i, y, new Color(0, 0, 0).getRGB());
                             }
                         }
                     }
-                }else{
-                    int y=y1;
+                } else {
+                    int y = y1;
                     for (int i = min(x1, x2); i < max(x1, x2); i++) {
                         img.setRGB(i, y, new Color(0, 0, 0).getRGB());
                     }
                 }
-            }else{
+            } else {
                 int y = 768;
-                for (int i = min(y1, y2); i <max(y1, y2); i++) {
-                    for (int j = min(x1, x2); j <max(x1, x2); j++) {
+                for (int i = min(y1, y2); i < max(y1, y2); i++) {
+                    for (int j = min(x1, x2); j < max(x1, x2); j++) {
                         if (i == (((j - x1) * (y2 - y1)) / (x2 - x1)) + y1) {
                             y = j;
                             img.setRGB(j, i, new Color(0, 0, 0).getRGB());
@@ -62,73 +50,39 @@ public class Render {
                     }
                 }
             }
-        }else{
+        } else {
             for (int i = min(y1, y2); i < max(y1, y2); i++) {
                 img.setRGB(x1, i, new Color(0, 0, 0).getRGB());
             }
         }
     }
 
-    public static void renderTriangle1(BufferedImage img, int x1, int y1, int x2, int y2, int x3, int y3){
-        int max_y=max(y1, max(y2, y3));
-        int max_x=max(x1, max(x2, x3));
-        int min_x=min(x1, min(x2, x3));
-        int min_y=min(y1, min(y2, y3));
-        int r=0;
-        double a= (double)((x2-x1)*(y3-y1)-(y2-y1)*(x3-x1));
-        for (int x = min_x; x <= max_x; x++) {
-            r++;
-            for (int y = min_y; y <= max_y; y++) {
-                Color c= new Color(r, 0, 0);
-                double u=((double)((x-x1)*(y3-y1)-(x3-x1)*(y-y1)))/(a);
-                double v=((double)((x2-x1)*(y-y1)-(x-x1)*(y2-y1)))/(a);
-                if(((u+v)<=1)&(u>=0)&(v>=0)){
-                    img.setRGB(x, y, c.getRGB());
-                }
-            }
-        }
-    }
-    public static void renderTriangle(BufferedImage img, double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3,double z3) {
+    public static void renderTriangle(BufferedImage img, double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3) {
         {
-            double li[] = new double[3];
-            li[0] = 1366;
-            li[1] = 786;
-            double z[] = new double[3];
-            z = vectorm1(x1 - x2, x1 - x3, y1 - y2, y1 - y3, z1 - z2, z1 - z3);
-            z = normalize(z);
-            double k[] = new double[3];
-            k[0] = 1366 - x1;
-            k[1] = 786 - y1;
-            k[2] = 10 - z1;
-            k = normalize(k);
-            double t;
-
-            double t1 = k[0] * z[0] + k[1] * z[1] + k[2] * z[2];
-            double t2 = k[0] * z[0] + k[1] * z[1] + k[2] * z[2];
-            double t3 = k[0] * z[0] + k[1] * z[1] + k[2] * z[2];
-            t = k[0] * z[0] + k[1] * z[1] + k[2] * z[2];
-            for (int x =(int) (Math.min(x1, Math.min(x2, x3))); x <= (int) (Math.max(x1, Math.max(x2, x3))); x++) {
-                for (int y = (int)(Math.min(y1, Math.min(y2, y3))); y < (int)(Math.max(y1, Math.max(y2, y3))); y++) {
-                    double AB[] = {x2-x1,y2-y1,z2-z1};
-                    double AC[] = {x3-x1,y3-y1,z3-z1};
-                    double PA[] = {x1-x,y1-y};
-                    double u1[]={AB[0],AC[0],PA[0]};
-                    double u2[]={AB[1],AC[1],PA[1]};
-                    double V[] = Render.vectorm2(u1,u2);
-                    double u = V[0]/V[2]; double v = V[1]/V[2];
-                    if (u + v <= 1 && u >= 0 && v >= 0){
-                        t=(t3*u+t2*v+t1*(1-u-v))/3;
-
-                        if (t<0){
-                            img.setRGB((int)(1366-x)%255, (int)(786-y)%255, new Color(0, (int) 0, (int) 0).getRGB());
+            for (int x = (int) (Math.min(x1, Math.min(x2, x3))); x <= (int) (Math.max(x1, Math.max(x2, x3))); x++) {
+                for (int y = (int) (Math.min(y1, Math.min(y2, y3))); y < (int) (Math.max(y1, Math.max(y2, y3))); y++) {
+                    double AB[] = {x2 - x1, y2 - y1, z2 - z1};
+                    double AC[] = {x3 - x1, y3 - y1, z3 - z1};
+                    double PA[] = {x1 - x, y1 - y};
+                    double u1[] = {AB[0], AC[0], PA[0]};
+                    double u2[] = {AB[1], AC[1], PA[1]};
+                    double z[] = new double[3];
+                    z[0] = u1[2]* u2[1] - u1[1] * u2[2];
+                    z[1] = u1[0] * u2[2] - u1[2] * u2[0];
+                    z[2] = u1[1] * u2[0] - u1[0] * u2[1];
+                    double u = z[0] / z[2];
+                    double v = z[1] / z[2];
+                    if (u + v <= 1 && u >= 0 && v >= 0) {
+                        if (x >= 0 & y >= 0) {
+                            img.setRGB(x, y, new Color(0, 0, 0).getRGB());
                         }
-                        else
-                            img.setRGB((int)(1366-x)%255, (int)(786-y)%255, new Color((int) (t*255), (int) (t*255), (int) ((t)*255)).getRGB());
                     }
                 }
             }
         }
     }
+
+
     //вектора
     //сумма, она же разность, если v2 брать с отрицательными координатами
     public static int[] addition(int[] v1, int[] v2){
@@ -148,45 +102,54 @@ public class Render {
         return res;
     }
 
+    //произведение двух трехмерных векторов
+    public static double[] multiplication(double[] v1, double[] v2){
+        double[] res=new double[3];
 
-    public static double[] vectorm1 ( double x1, double y1, double x2, double y2, double x3, double y3)
-    {
-        double z[] = new double[3];
-        z[0] = x3 * y2 - x2 * y3;
-        z[1] = x1 * y3 - x3 * y1;
-        z[2] = x2 * y1 - x1 * y2;
-        return z;
+        res[0]=v1[1]*v2[2]-v1[2]*v2[1];
+        res[1]=v1[2]*v2[0]-v1[0]*v2[2];
+        res[2]=v1[0]*v2[1]-v1[1]*v2[0];
+
+        return res;
     }
 
-    public static int multiplication(int[] v1, int[] v2){
-        int res=0;
-        for (int i = 0; i < v1.length; i++) {
-            res+=v1[i]*v2[i];
+    //нормализация
+    public static double[] normalize(double[] v){
+        double res[]=new double[v.length];
+        double z=0;
+        for (int i = 0; i < v.length; i++) {
+            z+=v[i]*v[i];
+        }
+        for (int i = 0; i < v.length; i++) {
+            res[i]=v[i]/(Math.sqrt(z));
         }
         return res;
     }
 
-    //нормаль
-    public static double[] normalize(double[] v){
-        double res[]=new double[3];
-        res[0] = v[0] / Math.sqrt(v[0] * v[0] + v[1] * v[1]+v[2]*v[2]);
-        res[1] = v[1] / Math.sqrt(v[0] * v[0] + v[1] * v[1]+v[2]*v[2]);
-        res[2] = v[2] / Math.sqrt(v[0] * v[0] + v[1] * v[1]+v[2]*v[2]);
+    //скалярное произведение векторов размерности 3
+    public static double scal_multiplication(double[] v1, double[] v2){
+        double res=0;
+
+        for (int i = 0; i < v1.length; i++) {
+            res+=v1[i]*v2[i];
+        }
+
         return res;
     }
 
-    //произвеение трех векторов
-    public static double[] vectorm2 ( double x [],double y[])
-    {
-        double z[] = new double[3];
-        z[0] = x[2]* y[1] - x[1] * y[2];
-        z[1] = x[0] * y[2] - x[2] * y[0];
-        z[2] = x[1] * y[0] - x[0] * y[1];
-        return z;
+    //умножение матрицы 3x3 на вектор длины 3
+    public static double[] matrixandvector(double[][]m, double[] v){
+        double[]res=new double[3];
+
+        for (int i = 0; i < 3; i++) {
+            res[i]+=m[i][0]*v[0]+m[i][1]*v[1]+m[i][2]*v[2];
+        }
+
+        return res;
     }
 
     //матрицы
-    //умножение
+//умножение
     public static int[][] matrixmultiplication(int[][] m1, int[][]m2){
         int[][]res=new int[m1.length][m2[0].length];
         if(m1[0].length==m2.length){
